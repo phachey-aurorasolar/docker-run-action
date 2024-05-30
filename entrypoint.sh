ad35751edbef3579f2b2ec910dfb84dc1ad87a90
phachey-aurorasolar <129956104+phachey-aurorasolar@users.noreply.github.com>
2024-05-30 14:16:17 -0300

    Added AWS ECR login

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-if [ ! -z $INPUT_USERNAME ];
-then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
+# ECR URI regex pattern
+ecr_regex_pattern="^[0-9]{12}\.dkr\.ecr\.[a-z0-9-]+\.amazonaws\.com\/.+"
+
+if [[ $INPUT_IMAGE =~ $ecr_regex_pattern ]]; then
+aws ecr get-login-password --region $INPUT_AWS_REGION | docker login --username AWS --password-stdin $INPUT_REGISTRY
+elif [ ! -z $INPUT_USERNAME ]; then
+echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
 fi
 
 if [ ! -z $INPUT_DOCKER_NETWORK ];
